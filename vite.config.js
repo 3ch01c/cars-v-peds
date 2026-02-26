2c71bd215b9b26ea58481a8f17e292fb0bf93b56
Jack Miner <5547581+3ch01c@users.noreply.github.com>
2026-02-25 20:42:59 -0700

    Update GitHub Pages base path for repo rename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cars-and-peds/',
+  base: '/cars-v-peds/',
   plugins: [react()],
 })
